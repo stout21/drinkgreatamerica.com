adbca222aeb6c947a19e9d1d071be7f9ee47d939
Dustan Kasten <dustan.kasten@gmail.com>
2013-12-07 22:40:33 -0500

    Products: desktop done with the ol' javascript

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,57 @@
-(function(FastClick) {
+(function(document, FastClick) {
+
   'use strict';
 
   FastClick.attach(document.body);
 
+
+  // Hook up the flavors section
+
   var TAB_IS_ACTIVE = 'tab--is-active';
-  document.addEventListener('click', function(e) {
-    if (e.target.matches('.tab-link')) {
-      e.preventDefault();
+  var TAB_IS_INACTIVE = 'tab--is-inactive';
 
-      var target = e.target.href.split('#');
-      if (target) target = document.getElementById(target[1]);
-      if (target) {
-        e.target.parentNode.querySelector('.' + TAB_IS_ACTIVE).classList.remove(TAB_IS_ACTIVE);
-        target.classList.add(TAB_IS_ACTIVE);
-      }
+  $('#flavors').addEventListener('click', function(e) {
+    if (!e.target.matches('.tab-link')) return;
+    e.preventDefault();
+
+    var target = $(e.target.getAttribute('href')),
+        parent;
+
+    if (target) {
+      parent = e.target.parentNode;
+
+      // reset all the tabs
+      $$('.' + TAB_IS_ACTIVE, e.target.parentNode).forEach(function(el) {
+        el.classList.remove(TAB_IS_ACTIVE);
+      });
+
+      // set the new tabs to active
+      e.target.className = 'tab-link ' + TAB_IS_ACTIVE;
+      target.classList.add(TAB_IS_ACTIVE);
+      target.classList.remove(TAB_IS_INACTIVE);
+
+      // set everything else to inactive
+      $$('.tab', e.target.parentNode).forEach(function(el) {
+        if (el.matches('.' + TAB_IS_ACTIVE)) return;
+        el.classList.add(TAB_IS_INACTIVE);
+      });
+
     }
-  })
+  });
 
-})(window.FastClick);
+  // jQueryLite yo.
+
+  function $(selector, ctx) {
+    if (selector[0] === '#') return document.getElementById(selector.substr(1));
+    return (ctx || document).querySelector(selector);
+  }
+
+  function $$(selector, ctx) {
+    return toArray((ctx || document).querySelectorAll(selector));
+  }
+
+  function toArray(like) {
+    return Array.prototype.slice.call(like, 0);
+  }
+
+})(document, window.FastClick);
