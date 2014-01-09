7ccf8da73f3ca218baee80a3c286d7dc28af4cef
Dustan Kasten <dustan.kasten@gmail.com>
2014-01-08 23:37:46 -0500

    Bit of jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,11 @@ module.exports = function (grunt) {
     jshint: {
       options: {
         jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
+        reporter: require('jshint-stylish'),
+        ignores: [
+          '<%= yeoman.app %>/scripts/locations.js',
+          '<%= yeoman.app %>/scripts/polyfills.js',
+        ]
       },
       all: [
         'Gruntfile.js',
