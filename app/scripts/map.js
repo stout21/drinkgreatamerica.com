088b5c388a62141bbd0763b6b5bca07da6962d9e
Dustan Kasten <dustan.kasten@gmail.com>
2016-10-24 14:09:49 +0000

    Initial request should be based on what is in the input field, fix InfoWindow

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -2,25 +2,32 @@
 (function(exports, document, fetch) {
   'use strict';
   
-  var query = [
-    'query {',
-      // customize the zip based on what the user is looking at
-      'locations(brand:GREAT_AMERICA, zip:280336) {',
-        // all the fields we care about for our UI
-        'name street city state zip lat long distance',
+  function getQuery (postalCode) {
+    return [
+      'query {',
+        // customize the zip based on what the user is looking at
+        'locations(brand:GREAT_AMERICA, zip:' + postalCode + ') {',
+          // all the fields we care about for our UI
+          'name street city state zip lat long distance',
+        '}',
       '}',
-    '}',
-  ];
-  var protocol = location.protocol !== 'file:' ? location.protocol : 'http:';
-  fetch(location + '//stout-brand-finder.herokuapp.com/graphql?query=' + encodeURIComponent(query.join('')))
-    .then(resp => resp.json())
-    .then(data => {
-      console.log(data);
-      setup(data.data.locations);
-      return data;
-    })
-    .then(data => exports[MAIN](data))
-    .catch(err => console.log(err));
+    ].join('');
+  }
+  
+  function request(postalCode) {
+    var protocol = location.protocol !== 'file:' ? location.protocol : 'http:';
+    fetch(protocol + '//stout-brand-finder.herokuapp.com/graphql?query=' + encodeURIComponent(getQuery(postalCode)))
+      .then(function(resp) { return resp.json(); })
+      .then(function (data) {
+        setup(data.data.locations);
+        return data;
+      })
+      .then(function (data) { return exports[MAIN](data); })
+      .catch(function (err) {
+        console.log(err);
+        return err;
+      });
+  }
 
   var DEFAULT_LAT_LONG = [35.8278, -78.6421]; // Raleigh, NC
   var MAIN = '_buildThatMap'; // main func. called when google maps js loads
@@ -29,44 +36,47 @@
   var mapManager, isReady = false;
 
   function setup(locations) {
-    if (!isReady) return setTimeout(function() { setup(locations); }, 10);
-
-    mapManager = new MapManager(locations);
-    mapManager.template(MapManager.INFO_WINDOW, function(data) {
-      return [
-        /*jshint ignore:start*/
-        '<article class="info-window">',
-          '<h3 class="info-store">', data.store, '</h3>',
-          '<span class="info-address">', data.address, '</span>',
-          '<span class="info-city">', data.city, '</span>, ',
-          '<span class="info-state">', data.state, '</span> ',
-          '<span class="info-zip">', data.zip, '</span>',
-        '</article>'
-        /*jshint ignore:end*/
-      ].join('');
-    });
-
-    mapManager.template(MapManager.LOCATIONS_LIST, function(data) {
-      return data.map(function(loc) {
+    if (!isReady || typeof google === 'undefined') return setTimeout(function() { setup(locations); }, 10);
+    
+    if (!mapManager) {
+      mapManager = new MapManager(locations);
+      mapManager.template(MapManager.INFO_WINDOW, function(data) {
         return [
           /*jshint ignore:start*/
-          '<div class="location" data-id="', loc._id , '">',
-            '<h3 class="location-store">', loc.name, '</h3>',
-            '<span class="location-address">', loc.address, '</span>',
-            '<span class="location-city">', loc.city, '</span>, ',
-            '<span class="location-state">', loc.state, '</span> ',
-            '<span class="location-zip">', loc.zip, '</span>',
-          '</div>',
+          '<article class="info-window">',
+            '<h3 class="info-store">', data.name, '</h3>',
+            '<span class="info-address">', data.street, '</span>',
+            '<span class="info-city">', data.city, '</span>, ',
+            '<span class="info-state">', data.state, '</span> ',
+            '<span class="info-zip">', data.zip, '</span>',
+          '</article>'
           /*jshint ignore:end*/
         ].join('');
-      }).join('');
-    });
+      });
 
+      mapManager.template(MapManager.LOCATIONS_LIST, function(data) {
+        return data.map(function(loc) {
+          return [
+            /*jshint ignore:start*/
+            '<div class="location" data-id="', loc._id , '">',
+              '<h3 class="location-store">', loc.name, '</h3>',
+              '<span class="location-address">', loc.address, '</span>',
+              '<span class="location-city">', loc.city, '</span>, ',
+              '<span class="location-state">', loc.state, '</span> ',
+              '<span class="location-zip">', loc.zip, '</span>',
+            '</div>',
+            /*jshint ignore:end*/
+          ].join('');
+        }).join('');
+      });
+    }
+    
     mapManager.draw();
+    mapManager.setLocations(locations);
   }
 
   function initialize() {
-    //google.maps.visualRefresh = true;
+    // google.maps.visualRefresh = true;
     isReady = true;
   }
 
@@ -77,17 +87,29 @@
     this._locations = locations;
     this.locations = locations;
     this.searchElement = $('#filter-locations');
+    this.currentPostalCode = this.searchElement.value;
     this.locationsElement = $('#map-locations-list');
     var _this = this;
-    this.searchElement.addEventListener('keyup', function() {
-      _this.filter.apply(_this, arguments);
+    this.searchElement.addEventListener('blur', _this.search, false);
+    this.searchElement.addEventListener('keyup', function(event) {
+      if (event.which === 13) { // enter key
+        _this.search.apply(_this, arguments);
+      }
     }, false);
 
     this.map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 8,
-      center: new google.maps.LatLng(locations[0].lat, locations[0].long)
+      zoom: 11,
+      center: locations[0] && new google.maps.LatLng(locations[0].lat, locations[0].long)
       //center: new google.maps.LatLng(DEFAULT_LAT_LONG[0], DEFAULT_LAT_LONG[1])
     });
+    if (!locations || locations.length === 0) {
+      var self = this;
+      (new google.maps.Geocoder()).geocode({address: this.currentPostalCode}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          self.map.setCenter(results[0].geometry.location);
+        }
+      });
+    }
 
     this.infoWindow = new google.maps.InfoWindow({});
   }
@@ -101,7 +123,6 @@
       var self = this;
       var map = this.map;
       this.locations.forEach(draw);
-      this.filter({ target: this.searchElement });
 
       return this;
 
@@ -111,8 +132,6 @@
           title: loc.store,
           map: map
         });
-
-        loc._id = 'location-' + Math.floor(Math.random() * 100000);
 
         google.maps.event.addListener(loc.marker, 'click', select(loc));
       }
@@ -124,44 +143,12 @@
       }
     },
 
-    filter: function(e) {
-      var _this = this;
-      var map = this.map;
-      var query = new RegExp(e.target.value, 'ig');
-      var keys = 'store address city state zip'.split(' ');
-
-      // TODO: Maybe make this smarter
-      this.locations = this._locations; /*.filter(function(location) {
-        var i = keys.length, prop;
-        while (i--)
-          if ((prop = location[keys[i]]) && prop.match(query))
-            return location;
-      });*/ // TODO .sort() by relevance;
-
-      // Sync up those markers
-      this._locations.forEach(function(location, i) {
-        if (_this.locations.indexOf(location) === -1)
-          return location.marker.setMap(null);
-
-        if (location.marker.getMap() === null) {
-          setTimeout(animateIn(location), i * 16);
-        }
-      });
-
-      function animateIn(location) {
-        return function() {
-          location.marker.setMap(map);
-          location.marker.setAnimation(google.maps.Animation.DROP);
-        };
-      };
-
-      var str = [
-        '<div id="location-results-count">',
-          '<span class="count">', this.locations.length, '</span>',
-          '<span class="total">', this._locations.length, '</span>',
-        '</div>'
-      ].join('') + this.render(MapManager.LOCATIONS_LIST, this.locations);
-      this.locationsElement.innerHTML = str;
+    search: function(event) {
+      var postalCode = event.target.value;
+      if (postalCode.length === 5) {
+        this.currentPostalCode = postalCode;
+        request(postalCode);
+      }
     },
 
     select: function(location) {
@@ -173,6 +160,36 @@
       this.locationsElement.querySelector('[data-id="' + location._id + '"]').className = 'location location--isActive';
 
       return this;
+    },
+    
+    setLocations: function(locations) {
+      if (!locations || locations.length === 0) {
+        var self = this;
+        (new google.maps.Geocoder()).geocode({address: this.currentPostalCode}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            self.map.setCenter(results[0].geometry.location);
+          }
+        });
+      }
+      // clear previous locations
+      this.map = new google.maps.Map(document.getElementById('map-canvas'), {
+        zoom: 11,
+        center: locations[0] && new google.maps.LatLng(locations[0].lat, locations[0].long)
+      });
+      
+      this.locations = locations || this._locations;
+      locations.forEach(function(loc) {
+        loc._id = 'location-' + Math.floor(Math.random() * 100000);
+      });
+
+      var str = this.render(MapManager.LOCATIONS_LIST, this.locations);
+      this.locationsElement.innerHTML = str || [
+        '<div class="location">',
+          '<h3 class="location-store">Sorry!</h3>',
+          '<span class="location-address">We don’t currently distribute to this area.</span>',
+        '</div>'
+      ].join('');
+      this.draw();
     },
 
     template: function(name, fn) {
@@ -192,6 +209,7 @@
 
   exports[MAIN] = initialize;
   exports[SETUP] = setup;
+  request($('#filter-locations').value);
 
 })(window, document, fetch);
 
